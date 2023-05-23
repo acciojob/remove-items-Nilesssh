49549606e1b3f57d59bb9b3faa68778e052a40e7
Nilesssh <103926034+Nilesssh@users.noreply.github.com>
2023-05-24 01:51:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let selectedValue = document.querySelector("#colorSelect");
+let select = document.querySelector("input");
+select.addEventListener("click" ,  data => {
+	selectedValue.remove(data.selectedIndex)
+	
+})
